ea567b5cc578480368b82ebe6acb57f99c270900
GeorgeKVR <67703102+GeorgeKVR@users.noreply.github.com>
2021-10-05 21:54:38 +0400

    minor style change

--- js/draw.js
+++ js/draw.js
@@ -9,8 +9,8 @@
     '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
     'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 'DEL',
     'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", '\',', 'Enter',
-    'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift',
-    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→'
+    'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '▲', 'Shift',
+    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
   ]
 
   const keyBoard = document.createElement('div')
