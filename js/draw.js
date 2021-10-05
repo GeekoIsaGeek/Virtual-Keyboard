a227a408574ea97739c29cde5c41c803e90a7cf3
GeorgeKVR <67703102+GeorgeKVR@users.noreply.github.com>
2021-10-06 02:55:30 +0400

    Add files via upload

--- js/draw.js
+++ js/draw.js
@@ -1,10 +1,8 @@
 (function () {
-  
   const input = document.createElement('textarea')
   input.className = 'textarea'
   document.body.appendChild(input)
-
-  
+ 
   const keys = [
     '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
     'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 'DEL',
